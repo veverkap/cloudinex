ee30bd7bacbbc090c72e6094290ba06b8bec4ad9
Patrick Veverka <patrick.veverka@gmail.com>
2017-08-22 22:34:17 -0400

    ignoring

--- config/prod.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :cloudinex,
-       base_url: "https://api.cloudinary.com/v1_1/",
-       api_key: "317572533423569",
-       secret: "4ry1Be3_4yHMvWq8g-bJIubx6ZE",
-       cloud_name: "push-comedy-theater"
--- config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :cloudinex,
-       debug: false,
-       base_url: "https://api.cloudinary.com/v1_1/",
-       api_key: "111111111111111",
-       secret: "yyyyyyy_yyyyyyyy-yyyyyyyyy",
-       cloud_name: "demo"
