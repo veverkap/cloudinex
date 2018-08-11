92f511fbb6473dde8eadd7d4b04fead651335aaf
Patrick Veverka <patrick.veverka@gmail.com>
2018-08-11 19:01:41 -0400

    bumping travis

--- mix.exs
+++ mix.exs
@@ -1,13 +1,13 @@
 defmodule Cloudinex.Mixfile do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
 
   def project do
     [
       app: :cloudinex,
       version: @version,
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
